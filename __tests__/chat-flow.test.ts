c60fc77083a109798f190bd9379229fedbdb157a
Yoseph <yosephten@gmail.com>
2025-09-20 15:44:29 +0300

    `test`: ✅ adding tests for execute tool

--- __tests__/chat-flow.test.ts
+++ __tests__/chat-flow.test.ts
@@ -1,7 +1,8 @@
 import { requestModel } from "@/src/modules/core/lib/services/model-request.service";
+import executeTool from "@/src/modules/core/lib/services/tool.service";
 
 describe("Full Flow Test", () => {
-  it("requestModel", async () => {
+  it("requestModel + executeTool", async () => {
     const response = await requestModel({
       model: "gemini",
       query: "What is the weather in Addis Ababa right now?",
@@ -9,5 +10,12 @@ describe("Full Flow Test", () => {
     expect(response).toBeTruthy();
     expect(response).toHaveProperty("arguments");
     expect(response).toHaveProperty("tool");
+
+    if (response) {
+      const toolResponse = await executeTool(response);
+      expect(toolResponse).toBeTruthy();
+      expect(toolResponse?.success).toBeTruthy();
+      expect(toolResponse).toHaveProperty("response");
+    }
   });
 });
