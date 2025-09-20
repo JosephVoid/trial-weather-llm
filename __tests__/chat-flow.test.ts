2725ab3d4a62292c40852cfa4d7a270c09edbe1d
Yoseph <yosephten@gmail.com>
2025-09-20 17:29:13 +0300

    `test`: ✅ adding tests for feedModel

--- __tests__/chat-flow.test.ts
+++ __tests__/chat-flow.test.ts
@@ -1,8 +1,11 @@
-import { requestModel } from "@/src/modules/core/lib/services/model-request.service";
+import {
+  feedModel,
+  requestModel,
+} from "@/src/modules/core/lib/services/model-request.service";
 import executeTool from "@/src/modules/core/lib/services/tool.service";
 
 describe("Full Flow Test", () => {
-  it("requestModel + executeTool", async () => {
+  it("requestModel + executeTool + feedModel", async () => {
     const response = await requestModel({
       model: "gemini",
       query: "What is the weather in Addis Ababa right now?",
@@ -16,6 +19,16 @@ describe("Full Flow Test", () => {
       expect(toolResponse).toBeTruthy();
       expect(toolResponse?.success).toBeTruthy();
       expect(toolResponse).toHaveProperty("response");
+
+      if (toolResponse) {
+        const userResponse = await feedModel({
+          model: "gemini",
+          feed: toolResponse.response,
+        });
+        expect(userResponse?.response).toBeTruthy();
+        expect(userResponse?.success).toBeTruthy();
+        expect(userResponse).toHaveProperty("response");
+      }
     }
   });
 });
