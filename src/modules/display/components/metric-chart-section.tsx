7a4be3f80a86445003d7c347e846ffdc8093f4e9
Yoseph <yosephten@gmail.com>
2025-09-22 16:29:42 +0300

    `fix`: 🐛 prevent re-comparison on deployed code

--- src/modules/display/components/metric-chart-section.tsx
+++ src/modules/display/components/metric-chart-section.tsx
@@ -25,6 +25,8 @@ export default function MetricChartSection() {
       latency: 1177.6,
     },
   ]);
+  const [alert, setAlert] = React.useState<string | null>(null);
+
   const { run: compareMetrics, loading: comparisonLoading } =
     useAsync(fetchMetricsAction);
 
@@ -88,6 +90,18 @@ export default function MetricChartSection() {
   };
 
   const handleModelComparison = async () => {
+    if (
+      !window.location.hostname.includes("localhost") &&
+      !window.location.hostname.includes("127.0.0.1")
+    ) {
+      setAlert(`Comparing these models will consume alot of tokens and weather API credits, and may make the chat bot above stop working.\n 
+                Thus I kindly recommend you to pull this code, put in your own API keys and run this comparison.\n 
+                Thank you for understanding! 🙏`);
+      setTimeout(() => {
+        setAlert(null);
+      }, 12000);
+      return;
+    }
     const response = await compareMetrics();
     if (response) {
       console.log({ response });
@@ -98,9 +112,27 @@ export default function MetricChartSection() {
 
   return (
     <div
-      className="w-full h-screen flex items-center justify-center"
+      className="w-full h-screen flex items-center justify-center relative"
       id="chart-section"
     >
+      {alert && (
+        <div role="alert" className="alert alert-warning absolute top-0 w-1/2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          <span>{alert}</span>
+        </div>
+      )}
       <div className="flex flex-col w-full p-5 gap-6">
         <button className="btn btn-ghost w-2/8" onClick={handleModelComparison}>
           {comparisonLoading ? <Spinner /> : "Re-Compare Models"}
